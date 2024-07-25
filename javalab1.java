fa20d3eedf0c0e8abc30f141febc9adc7b3a7f9a
Lohithaswin <lohithaswin.v2023@vitstudent.ac.in>
2024-07-25 20:03:14 +0530

    Add files via upload

---
+++ javalab1.java
@@ -0,0 +1,36 @@
+//JAVA LAB 1 
+
+//1)Converting int to double
+public class Main{
+public static void main(String[]args){
+int i=19;  
+double d=i; //converting int to double. 
+System.out.println(i);
+System.out.println(d);}
+}
+
+//2)Explicit cating double to int
+public class Main{
+public static void main(String[]args){
+double d=19.97;
+int i=(int)d; //explicit casting double to int
+System.out.println(i);
+System.out.println(d);}
+}
+//3.
+ public class Main{
+public static void main(String[]args){
+int d=19;
+int sum;
+int k=190;
+sum=k+d;//adding 2 variables
+int sum1=30+101;//adding 2 ints
+String s="hello";
+System.out.println(sum);
+System.out.println(sum1);
+ 
+    System.out.println(s+k);//adding string to int
+}
+}
+
+
