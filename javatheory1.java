d162b7ee6489912b30fc337c61064a3bf015e4a7
Lohithaswin <lohithaswin.v2023@vitstudent.ac.in>
2024-07-25 20:19:40 +0530

    Add files via upload

---
+++ javatheory1.java
@@ -0,0 +1,56 @@
+//JAVA THEORY1
+
+
+//Concatenation of strings
+ public class Main{
+public static void main(String args[]){
+String a="Hello";
+String b= "World";
+System.out.println(a+b);}}
+
+//Using concat() method
+ public class Main{
+public static void main(String args[]){
+String a="Hello";
+String b= "Worl d";
+System.out.println(a.concat(b));}}
+
+//Adding string with int
+ public class Main{
+public static void main(String args[]){
+String a="10";
+int b=20;
+String z=a+b;
+System.out.println(z);}}
+
+// Escape character 
+public class Main{
+public static void main(String args[]){
+String z="We have - a \"hulk\"";
+System.out.println(z);}}
+
+//Max of 2 numbers
+public class Main{
+public static void main(String args[]){
+System.out.println(Math.max(5,10));}}
+
+//Finding minimum
+public class Main{
+public static void main(String args[]){
+System.out.println(Math.min(5,10));}}
+
+//SQUARE ROOT
+public class Main{
+public static void main(String args[]){
+System.out.println(Math.sqrt(10));}}
+
+//ABSOLUTE VALUE
+public class Main{
+public static void main(String args[]){
+System.out.println(Math.abs(-10.34));}}
+
+//Random Value
+public class Main{
+public static void main(String args[]){
+System.out.println(Math.random());}}
+
